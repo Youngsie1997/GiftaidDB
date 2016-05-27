b7b2f376dd7454857d6a04f11694d8583c6bb992
Youngsie1997 <Youngsie1997@gmail.com>
2016-05-27 02:39:00 +0100

    Added update button To change the status and last_modified_date by id, did quick fix to prevent non-numbers in  giftaid tb

--- GiftaidDB/MainWindow.xaml.cs
+++ GiftaidDB/MainWindow.xaml.cs
@@ -187,9 +187,66 @@ namespace GiftaidDB
 
         private void tbGiftAid_Number_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.Key == Key.Return)
+            if (e.Key == Key.Return)
             {
                 cbStatus.Focus();
+            }
+
+            switch (e.Key)
+            {
+                case Key.D0:
+                case Key.D1:
+                case Key.D2:
+                case Key.D3:
+                case Key.D4:
+                case Key.D5:
+                case Key.D6:
+                case Key.D7:
+                case Key.D8:
+                case Key.D9:
+                case Key.NumLock:
+                case Key.NumPad0:
+                case Key.NumPad1:
+                case Key.NumPad2:
+                case Key.NumPad3:
+                case Key.NumPad4:
+                case Key.NumPad5:
+                case Key.NumPad6:
+                case Key.NumPad7:
+                case Key.NumPad8:
+                case Key.NumPad9:
+                case Key.Back:
+                case Key.Return:
+                    break;
+                default:
+                    e.Handled = true;
+                    break;
+
+            }
+        }
+
+        private void btUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbRemove.Text.ToString() != "")
+            {
+                try
+                {
+                    this.OpenConn();
+                    string sql = "UPDATE giftaid SET status = '" + cbStatus.Text.ToString() + "',last_modified_date = 'now' WHERE ID = '"+tbRemove.Text.ToString()+"' ";
+                    NpgsqlCommand updateCommand = new NpgsqlCommand(sql, conn);
+                    int success = updateCommand.ExecuteNonQuery();
+                    MessageBox.Show(Convert.ToString(success));
+                    this.CloseConn();
+                }
+                catch (Exception msg)
+                {
+                    MessageBox.Show(msg.ToString());
+                    this.CloseConn();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter the id of the record you would like to Update in the field bellow");
             }
         }
     }
